08f359392207d54c956e1fb32a177603a2a1d61e
QuackitsQuinn <ollieollieausum@gmail.com>
2023-02-17 13:36:18 -0600

    format readme + constant
    wfwyk

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # rustbow
+
 ## Funky terminal ranbow with random
+
 ### EPILEPSY WARNING
+
 I dont know if this is somthing that could trigger a seizure but i dont want that to accidentally happen soooo
--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,46 @@ use ansi_term::Color::RGB;
 use prisma::{Rgb, Hsv, FromColor};
 use angular_units::Deg;
 use std::{time::Duration,io::{stdout, Stdout}};
+// Change rate is how fast the color changes
+const CHANGE_RATE:f64 = 0.3;
+const BLOCK:bool = true;
 fn main() -> crossterm::Result<()>{
     let mut stdout = stdout();
-    execute!(stdout, Hide);
+    let res = execute!(stdout, Hide);
+    if res.is_err() {
+        println!("Error: {}", res.err().unwrap());
+    }
     let mut pgrmstate:bool = true;
     let mut color = Rgb::from_color(&Hsv::new(Deg(0 as f64), 1 as f64, 1 as f64));
-    while pgrmstate {
-        color = nextcol(color);
-        stdout = put_rand(color,stdout);
-        (pgrmstate,stdout) = end(stdout);
-    }
-    Ok(())
-}
-fn go_rand_pos(mut stdout:Stdout) -> Stdout{
-    let mut rng = rand::thread_rng();
     let size = size();
     let sizeur = size.unwrap();
     let mx = sizeur.1;
     let my = sizeur.0;
+    while pgrmstate {
+        color = nextcol(color);
+        stdout = put_rand(color,stdout,mx,my);
+        (pgrmstate,stdout) = end(stdout);
+    }
+    Ok(())
+}
+fn go_rand_pos(mut stdout:Stdout,mx:u16,my:u16) -> Stdout{
+    let mut rng = rand::thread_rng();
     let x = rng.gen_range(0..mx);
     let y = rng.gen_range(0..my);
     execute!(stdout,MoveTo(y as u16,x as u16));
     stdout
 }
-fn put_rand(color:Rgb<f64>,stdout:Stdout ) -> Stdout{
+fn put_rand(color:Rgb<f64>,stdout:Stdout,mx:u16,my:u16) -> Stdout{
     let char:String;
-    let rng = rand::thread_rng();
-    char = rng.sample_iter(&Alphanumeric)
-    .take(1)
-    .map(char::from).collect();
-    let stdo = go_rand_pos(stdout);
+    if !BLOCK {
+        let rng = rand::thread_rng();
+        char = rng.sample_iter(&Alphanumeric)
+        .take(1)
+        .map(char::from).collect();
+    } else {
+        char = "█".to_string();
+    }
+    let stdo = go_rand_pos(stdout,mx,my);
     let painted = paint(char.as_str(),color);
     print!("{}", painted);
     stdo
@@ -52,7 +62,7 @@ fn nextcol(color:Rgb<f64>) -> Rgb<f64> {
     let mut hsv = Hsv::from_color(&color);
     let mut thishue:Deg<f64> = hsv.hue();
     if thishue <= Deg(359 as f64) {
-        thishue = thishue + Deg(0.01);
+        thishue = thishue + Deg(CHANGE_RATE);
     } else {
         thishue = Deg(0 as f64);
     }
