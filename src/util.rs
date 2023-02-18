fa6f7cf4f84162afe9b28d826c647181a2d26216
QuackitsQuinn <ollieollieausum@gmail.com>
2023-02-18 16:51:47 -0600

    make exit on keypress maybe work

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,8 @@
 
+use std::{io::stdout, time::Duration, sync::mpsc};
+
 use ansi_term::Color::RGB;
+use crossterm::{execute, event::{poll, read}, cursor::Hide};
 use prisma::Rgb;
 use rand::{distributions::Alphanumeric, Rng,thread_rng, rngs::ThreadRng};
 use angular_units::Deg;
@@ -33,3 +36,10 @@ pub fn nextcol(color: Rgb<f64>, change_rate: f64) -> Rgb<f64> {
     Rgb::from_color(&hsv)
 }
 
+pub fn wait_for_keypress(tx: mpsc::Sender<bool>) {
+    let mut stdout = stdout();
+    
+        if read().is_ok() { // wait until a key is pressed. dont care about info.
+            tx.send(true).unwrap();
+        }
+}
