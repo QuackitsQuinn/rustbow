3c8662d83ae8a7baa7c4d0f71e3330eae78b5a59
QuackitsQuinn <ollieollieausum@gmail.com>
2023-02-21 10:40:08 -0600

    Move rainbow to separate method and add default config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate crossterm;
 extern crate prisma;
+
 mod util;
 mod conf;
 mod test;
 
 use angular_units::Deg;
+use conf::Config;
 use crossterm::{
     cursor::Hide,
     execute,
@@ -18,25 +20,27 @@ use std::{
 // TODO: Make all of these configurable with args + default config file
 
 
-// Hue change rate. changes per character
-const CHANGE_RATE: f64 = 0.001;
-// Saturation of the color
-const SATURATION: f64 = 1 as f64;
-// Value of the color
-const VALUE: f64 = 1 as f64;
-// Block mode. If true, it will use █ instead of random characters
-const BLOCK: bool = true;
+const DEFAULT_CONFIG: Config = Config {
+    block_mode: true,
+    change_rate: 0.001,
+    saturation: 1.0,
+    value: 1.0,
+};
 
 /// Main function 
 /// For now, it contains the main loop. It will be moved to a separate file later with better customizability.
 fn main() -> crossterm::Result<()> {
+    rainbow(DEFAULT_CONFIG)
+}
+
+fn rainbow(config:Config) -> crossterm::Result<()> {
     let mut stdout = stdout();
     let res = execute!(stdout, Hide);
     if res.is_err() {
         println!("Error: {}", res.err().unwrap());
     }
     // Create the inital color. The sat and val are never changed so they only need to be set once.
-    let mut color = Rgb::from_color(&Hsv::new(Deg(0 as f64), SATURATION, VALUE));
+    let mut color = Rgb::from_color(&Hsv::new(Deg(0 as f64), config.saturation, config.value));
     let size = size();
     let sizeur = size.unwrap();
     let mx = sizeur.1;
@@ -45,8 +49,8 @@ fn main() -> crossterm::Result<()> {
         util::wait_for_keypress();
     });
     loop {
-        color = util::nextcol(color, CHANGE_RATE);
-        stdout = util::put_rand(color, stdout, mx, my,BLOCK);
+        color = util::nextcol(color, config.change_rate);
+        stdout = util::put_rand(color, stdout, mx, my,config.block_mode);
         if thread.is_finished() {
             break; // Exit when a key is pressed
         }
